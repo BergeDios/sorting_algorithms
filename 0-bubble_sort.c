eec3bf41fd9fb1644090e66ff093415c213f270b
BergeDios <santiago.goyret@gmail.com>
2022-02-11 06:48:10 -0800

    repo creation and task 0 locally working

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort array of ints using bubble sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int counter = 0, comparer = 0, swaper, swaper2;
+
+	if (size < 2)
+		return;
+
+	for (counter = 0; counter < size; counter++)
+	{
+		for (comparer = 0; comparer < (size - 1); comparer++)
+		{
+			if(array[comparer] > array[comparer + 1])
+			{
+				swaper = array[comparer];
+				swaper2 = array[comparer + 1];
+				array[comparer] = swaper2;
+				array[comparer + 1] = swaper;
+				print_array(array, size);
+			}
+		}
+			
+	}
+}
