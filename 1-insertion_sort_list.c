8febe529b78846bffa86456d4a52e7bb3a600a28
BergeDios <santiago.goyret@gmail.com>
2022-02-11 16:08:13 -0800

    task 1 and 2 locally

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sort dlinked with insertion algorithm
+ * @list: pointer to head
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = *list;
+	listint_t *tmp = NULL;
+
+	while (current != NULL)
+	{
+		tmp = current->next;
+		while (tmp != NULL && tmp->prev != NULL && tmp->n < tmp->prev->n)
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->next->prev;
+			if (tmp->prev != NULL)
+			{
+				tmp->prev->next = tmp;
+			}
+			if (tmp->prev == NULL)
+			{
+				*list = tmp;
+			}
+			tmp->next->prev = tmp;
+			print_list(*list);
+		}
+		if (current->next == NULL)
+			break;
+		if (current->n < current->next->n)
+			current = current->next;
+	}
+}
